79c30387d34b0de2e6fbac8a3d29bb0339c0c566
SAI PRASAD <saiprasad1996@hotmail.com>
2017-07-17 22:57:27 +0530

    Added users model into rest

--- lazyrest/urls.py
+++ lazyrest/urls.py
@@ -1,6 +1,21 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
+from django.contrib.auth.models import User
+from rest_framework import routers,serializers,viewsets
+
 from .import views
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=User
+        fields = ('url','username','email','is_staff')
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users',UserViewSet)
+
 urlpatterns =[
-    url(r'^$',views.index,name='index'),
+    url(r'^',include(router.urls)),
 ]
