3a13f30cab317a29fce64e66ed654402bf9b6b25
saiprasad1996 <saiprasad1996@hotmail.com>
2018-04-28 10:44:35 -0400

    Url issue

--- please/urls.py
+++ please/urls.py
@@ -6,8 +6,8 @@ urlpatterns =[
     url(r'^$',views.pleaseInfo),
     url(r'^askus$',views.postquestion),
     url(r'^questions$',views.showQuestions),
-    url(r'^collab/auth',views.auth),
-    url(r'^collab/auth/redirect',views.collab_redirect),
+    url(r'^collab/auth$',views.auth),
+    url(r'^collab/auth/redirect$',views.collab_redirect),
 
 
 ]
