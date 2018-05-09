dd64a7968d617e19fac92305c1e8900b48c35106
Sai Prasad <saiprasad1996@hotmail.com>
2018-05-09 23:26:47 +0530

    Admin add

--- please/admin.py
+++ please/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SlackAskUs,Log,AuthenticatedSpaces
+from .models import SlackAskUs,Log,AuthenticatedSpaces,User
 # Register your models here.
 admin.site.register(SlackAskUs)
 admin.site.register(Log)
 admin.site.register(AuthenticatedSpaces)
+admin.site.register(User)
